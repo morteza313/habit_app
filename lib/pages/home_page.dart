2de0bf88a604188eb8fbfbd3ff449218ae4a23c4
morteza <mortezakhoshki@gmail.com>
2024-05-13 08:51:58 +0330

    cleeaning the code by creating myDrawer in another folder

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,26 +1,11 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:habits/theme/theme_provider.dart';
-import 'package:provider/provider.dart';
+import 'package:habits/components/my_drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      drawer: Drawer(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        child: Center(
-          child: CupertinoSwitch(
-            value: Provider.of<ThemeProvider>(context).isDarkMode,
-            onChanged: (value) {
-              Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
-            },
-          ),
-        ),
-      ),
-    );
+    return Scaffold(appBar: AppBar(), drawer: MyDrawer());
   }
 }
